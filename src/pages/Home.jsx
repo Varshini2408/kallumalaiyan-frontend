5bc5e417b778f50772fecc1189d5bd3bffd06012
Varshini2408 <gobirajvarshini@gmail.com>
2026-07-17 21:11:17 +0800

    fix categories route and link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -175,7 +175,7 @@ export default function Home() {
           </div>
           {categories.length > 4 && (
             <div style={{ textAlign: "right", marginBottom: "8px" }}>
-              <a href="/shop" style={{
+              <a href="/categories" style={{
                 fontSize: "12px", color: "#1A1714", textDecoration: "underline"
               }}>View All Categories</a>
             </div>
